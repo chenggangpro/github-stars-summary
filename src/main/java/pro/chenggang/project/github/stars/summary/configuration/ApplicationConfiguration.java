fec63f8ca0b1045db8804478bc3fb30e8f09640d
chenggangpro <chenggangpro@gmail.com>
2025-05-04 23:18:51 +0800

    add GitHubStarsSummaryProperties to configure properties

--- src/main/java/pro/chenggang/project/github/stars/summary/configuration/ApplicationConfiguration.java
+++ src/main/java/pro/chenggang/project/github/stars/summary/configuration/ApplicationConfiguration.java
@@ -1,11 +1,11 @@
 package pro.chenggang.project.github.stars.summary.configuration;
 
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
+import pro.chenggang.project.github.stars.summary.properties.GitHubStarsSummaryProperties;
 
 /**
  * @author Gang Cheng
@@ -16,13 +16,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class ApplicationConfiguration {
 
     @Bean
-    public WebClient webClient(WebClient.Builder builder, Environment environment) {
-        String githubToken = environment.getProperty("GITHUB-TOKEN");
-        if (StringUtils.isBlank(githubToken)) {
-            throw new IllegalArgumentException("Missing GITHUB-TOKEN in file github-token.properties");
-        }
+    @ConfigurationProperties(prefix = GitHubStarsSummaryProperties.PREFIX)
+    public GitHubStarsSummaryProperties gitHubStarsSummaryProperties() {
+        return new GitHubStarsSummaryProperties();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder, GitHubStarsSummaryProperties gitHubStarsSummaryProperties) {
         return builder.baseUrl("https://api.github.com")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + githubToken)
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + gitHubStarsSummaryProperties.getGithubToken())
                 .defaultHeader(HttpHeaders.ACCEPT, "application/vnd.github+json")
                 .defaultHeader("X-GitHub-Api-Version", "2022-11-28")
                 .build();
---
+++ src/main/java/pro/chenggang/project/github/stars/summary/properties/GitHubStarsSummaryProperties.java
@@ -0,0 +1,49 @@
+package pro.chenggang.project.github.stars.summary.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.InitializingBean;
+
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+/**
+ * @author Gang Cheng
+ * @version 0.1.0
+ * @since 0.1.0
+ */
+@Slf4j
+@Getter
+@Setter
+public class GitHubStarsSummaryProperties implements InitializingBean {
+
+    public static final String PREFIX = "github-stars-summary";
+
+    /**
+     * The github token for accessing github api
+     * <p>
+     * The fine-grained token must have the following permission set:
+     * "Starring" user permissions (read)
+     *
+     * @see <a href="https://docs.github.com/en/rest/activity/starring?apiVersion=2022-11-28#list-repositories-starred-by-the-authenticated-user" />
+     */
+    private String githubToken;
+
+    /**
+     * The optional regex pattern of repository name which need to be ignored
+     */
+    private Pattern ignoreRepoPattern;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (StringUtils.isBlank(githubToken)) {
+            throw new IllegalArgumentException("Missing GITHUB-TOKEN in file github-token.properties");
+        }
+        if (Objects.nonNull(ignoreRepoPattern.pattern()) && ignoreRepoPattern.pattern().isBlank()) {
+            log.info("The ignore repo pattern is blank, force the pattern to be null");
+            this.ignoreRepoPattern = null;
+        }
+    }
+}
